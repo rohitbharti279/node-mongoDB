da655fe2ae7be621a0f08d4ea5f95057681c4037
Rohit Bharti <rohitbharti279@gmail.com>
2023-10-02 16:51:05 +0530

    update

--- update.js
+++ update.js
@@ -2,7 +2,17 @@ const dbconnect = require("./mongodb-connection");
 
 const updateData = async () => {
     const db = await dbconnect();
-    console.log(db);
+
+    // // single update and top update the beginning of the database
+    // const result = await db.updateOne({brand: "NOkia"},{
+    //     $set: {category: "pc", company: "Patna", brand: "Gionee"}
+    // });
+
+    //Multiple update
+    const result = await db.updateMany({brand: "NOkia"},{
+        $set: {category: "pc", company: "Patna", brand: "Gionee"}
+    });
+    console.log(result);
 }
 
 updateData();
